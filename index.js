ea26b1e4db0fa29d21e86a78c062ea53eeb73b64
UdhayKrishna <udhayakumarkrishnamoorthy@gmail.com>
2018-07-26 20:43:36 +0530

    added new code

--- index.js
+++ index.js
@@ -2,14 +2,29 @@
 
 const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = dialogflow({debug: true});
 
-app.intent('Default Welcome Intent', (conv) => {
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+ app.intent('Default Welcome Intent', (conv) => {
   conv.ask('Welcome to number echo! Say a number.');
 });
 
 app.intent('Input Number', (conv, {num}) => {
   // extract the num parameter as a local string variable
   conv.close(`You said ${num}`);
+});
+
 });
