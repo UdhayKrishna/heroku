b4ab5fe81c2d3a83035a63a5378564b8bca8c96a
UdhayKrishna <udhayakumarkrishnamoorthy@gmail.com>
2018-07-26 19:36:33 +0530

    new file commit for echo test

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
-'use strict';
+"use strict";
 
-const {WebhookClient} = require('dialogflow-fulfillment');
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const restService = express();
 
-exports.dialogflowFirebaseFulfillment = (req, res) => {
-    
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-function welcome (agent) {
-    agent.add(`Welcome to Express.JS webhook!`);
-}
-
-function fallback (agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-}
-
-function WebhookProcessing(req, res) {
-    const agent = new WebhookClient({request: req, response: res});
-    console.info(`agent set`);
-
-    let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-// intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-    agent.handleRequest(intentMap);
-}
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 
-// Webhook
-app.post('/', function (req, res) {
-    console.info(`\n\n>>>>>>> S E R V E R   H I T <<<<<<<`);
-    WebhookProcessing(req, res);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
 
-app.listen(8080, function () {
-    console.info(`Webhook listening on port 8080!`)
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
 });
-
-}
