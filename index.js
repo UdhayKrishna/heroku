47806a13bfcd4e5c721ccabe3c89d3e280419843
UdhayKrishna <udhayakumarkrishnamoorthy@gmail.com>
2018-07-26 20:06:32 +0530

    updated code

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 "use strict";
 
-const express = require("express");
+const express = require('express')();
 const bodyParser = require("body-parser");
+const router = require('express').Router();
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const ApiAiApp = require('actions-on-google').ApiAiApp;
 
-const restService = express();
+//const restService = express();
 
+express.use(bodyParser.json({type: 'application/json'}));
+
+router.post('/card', (req, res) => {
+	const app = new ApiAiApp({request: req, response: res});
+	const WELCOME_INTENT = 'input.welcome';
+
+	const actionMap = new Map();
+	actionMap.set(WELCOME_INTENT, welcomeIntent);
+	app.handleRequest(actionMap);
+	
+});
+
+function welcomeIntent(app){
+	app.ask(app.buildRichResponse()
+	// Create a basic card and add it to the rich response
+	.addSimpleResponse('Math and prime numbers it is!')
+		.addBasicCard(app.buildBasicCard(`42 is an even composite number. It
+		 is composed of three distinct prime numbers multiplied together. It
+		 has a total of eight divisors. 42 is an abundant number, because the
+		 sum of its proper divisors 54 is greater than itself. To count from
+		 1 to 42 would take you about twenty-one…`)
+		 .setTitle('Math & prime numbers')
+		 .addButton('Read more')
+		 .setImage('https://example.google.com/42.png', 'Image alternate text')
+		)
+	);
+}
+
+express.use('/example', router);
+
+express.listen('8081', function () {
+  console.log('Example app is running')
+})
+
+
+/*
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -45,4 +86,9 @@ restService.post("/echo", function(req, res) {
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
+
+
+
 });
+
+*/
